78828a33cdbe885ac762a1430b2e48dae8d71b41
DungTien04 <167021991+DungTien04@users.noreply.github.com>
2025-02-06 00:57:12 +0700

    forgotpass

--- FPTBank/src/java/dal/UserDAO.java
+++ FPTBank/src/java/dal/UserDAO.java
@@ -1,10 +1,14 @@
 package dal;
 
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import model.Asset;
+import model.Customer;
+import model.Salary;
 import model.User;
 import java.sql.*;
 
@@ -248,5 +252,296 @@ public class UserDAO extends DBContext {
             st.executeUpdate();
         } catch (SQLException e) {
         }
+    }
+
+    public User checkUserByEmail(String email) {
+        String sql = "SELECT * FROM [User] WHERE Email = ?";
+
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setString(1, email);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                User userToAdd = new User(rs.getInt("UserID"),
+                        rs.getString("Username"),
+                        rs.getString("Password"),
+                        rs.getString("FullName"),
+                        rs.getString("Phone"),
+                        rs.getString("Email"),
+                        rs.getDate("DateOfBirth"),
+                        rs.getBoolean("Gender"),
+                        rs.getString("CCCD"),
+                        rs.getInt("RoleID"),
+                        rs.getBoolean("Status"),
+                        rs.getDate("CreatedAt"));
+                return userToAdd;
+            }
+        } catch (SQLException e) {
+        }
+
+        return null;
+    }
+
+    public List<Asset> selectAllAssets() {
+        List<Asset> assets = new ArrayList<>();
+
+        try {
+
+            String sql = "SELECT  * FROM Asset";
+            PreparedStatement st = connection.prepareStatement(sql);
+            ResultSet resultSet = st.executeQuery();
+
+            while (resultSet.next()) {
+                Asset asset = new Asset();
+                asset.setId(resultSet.getInt("AssetId"));
+                asset.setCustomerId(resultSet.getInt("CustomerId"));
+                asset.setImage(resultSet.getString("Image"));
+                asset.setDescription(resultSet.getString("Description"));
+                asset.setValue(resultSet.getBigDecimal("Value"));
+                asset.setVerification(resultSet.getBoolean("Verification"));
+                asset.setStatus(resultSet.getBoolean("Status"));
+                asset.setCreatedAt(resultSet.getTimestamp("CreatedAt"));
+                assets.add(asset);
+            }
+            return assets;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public List<Salary> selectAllSalary() {
+        List<Salary> salarys = new ArrayList<>();
+        try {
+
+            String sql = "SELECT  * FROM Salary";
+            PreparedStatement st = connection.prepareStatement(sql);
+            ResultSet resultSet = st.executeQuery();
+
+            while (resultSet.next()) {
+                Salary salary = new Salary();
+                salary.setId(resultSet.getInt("SalaryId"));
+                salary.setCustomerId(resultSet.getInt("CustomerId"));
+                salary.setImage(resultSet.getString("Image"));
+                salary.setDescription(resultSet.getString("Description"));
+                salary.setValue(resultSet.getBigDecimal("Value"));
+                salary.setVerification(resultSet.getBoolean("Verification"));
+                salary.setStatus(resultSet.getBoolean("Status"));
+                salary.setCreatedAt(resultSet.getTimestamp("CreatedAt"));
+                salarys.add(salary);
+            }
+            return salarys;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Customer getUserbyCid(int customerID) {
+
+        try {
+            String sql = "Select *\n"
+                    + "from Customer c \n"
+                    + "join [User] u on u.UserID = c.UserID\n"
+                    + "where c.CustomerId =?";
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setInt(1, customerID);
+            ResultSet rs = st.executeQuery();
+            if (rs.next()) {
+                Customer customer = new Customer();
+                customer.setCustomerid(rs.getInt("CustomerId"));
+                customer.setCreditscore(rs.getInt("CreditScore"));
+                customer.setBalance(rs.getBigDecimal("Balance"));
+                User user = new User(rs.getInt("UserID"),
+                        rs.getString("Username"),
+                        rs.getString("Password"),
+                        rs.getString("FullName"),
+                        rs.getString("Phone"),
+                        rs.getString("Email"),
+                        rs.getDate("DateOfBirth"),
+                        rs.getBoolean("Gender"),
+                        rs.getString("CCCD"),
+                        rs.getInt("RoleID"),
+                        rs.getBoolean("Status"),
+                        rs.getDate("CreatedAt"));
+                customer.setUser(user);
+                return customer;
+            }
+        } catch (Exception e) {
+        }
+
+        return null;
+    }
+
+    public boolean updateAsset(Asset asset) {
+        String sql = "UPDATE Asset SET CustomerId = ?, Image = ?, Description = ?, "
+                + "Value = ?, Verification = ?, Status = ?, CreatedAt = ? WHERE AssetId = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setInt(1, asset.getCustomerId());
+            preparedStatement.setString(2, asset.getImage());
+            preparedStatement.setString(3, asset.getDescription());
+            preparedStatement.setBigDecimal(4, asset.getValue());
+            preparedStatement.setBoolean(5, asset.isVerification());
+            preparedStatement.setBoolean(6, asset.isStatus());
+            preparedStatement.setTimestamp(7, new java.sql.Timestamp(asset.getCreatedAt().getTime()));
+            preparedStatement.setInt(8, asset.getId());
+
+            int rowsUpdated = preparedStatement.executeUpdate();
+            return rowsUpdated > 0; // Trả về true nếu có ít nhất một hàng được cập nhật
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false; // Trả về false nếu có lỗi xảy ra
+        }
+    }
+
+    public Asset getAssetById(int assetId) {
+
+        String sql = "SELECT * FROM Asset WHERE AssetId = ?";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setInt(1, assetId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                Asset asset = new Asset();
+                asset.setId(resultSet.getInt("AssetId"));
+                asset.setCustomerId(resultSet.getInt("CustomerId"));
+                asset.setImage(resultSet.getString("Image"));
+                asset.setDescription(resultSet.getString("Description"));
+                asset.setValue(resultSet.getBigDecimal("Value"));
+                asset.setVerification(resultSet.getBoolean("Verification"));
+                asset.setStatus(resultSet.getBoolean("Status"));
+                asset.setCreatedAt(resultSet.getTimestamp("CreatedAt"));
+                return asset;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public List<Asset> getAssetsSortedByValue(String ascending) throws SQLException {
+        List<Asset> assets = new ArrayList<>();
+        String query = "SELECT * FROM Asset ORDER BY Value " + ascending;
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                Asset asset = new Asset();
+                asset.setId(rs.getInt("AssetId"));
+                asset.setCustomerId(rs.getInt("CustomerId"));
+                asset.setImage(rs.getString("Image"));
+                asset.setDescription(rs.getString("Description"));
+                asset.setValue(rs.getBigDecimal("Value"));
+                asset.setVerification(rs.getBoolean("Verification"));
+                asset.setStatus(rs.getBoolean("Status"));
+                asset.setCreatedAt(rs.getTimestamp("CreatedAt"));
+                assets.add(asset);
+            }
+            return assets;
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+        return null;
+
+    }
+
+    public List<Asset> getAssetsSortedByDate(String ascending) throws SQLException {
+        List<Asset> assets = new ArrayList<>();
+        String query = "SELECT * FROM Asset ORDER BY CreatedAt " + ascending;
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                Asset asset = new Asset();
+                asset.setId(rs.getInt("AssetId"));
+                asset.setCustomerId(rs.getInt("CustomerId"));
+                asset.setImage(rs.getString("Image"));
+                asset.setDescription(rs.getString("Description"));
+                asset.setValue(rs.getBigDecimal("Value"));
+                asset.setVerification(rs.getBoolean("Verification"));
+                asset.setStatus(rs.getBoolean("Status"));
+                asset.setCreatedAt(rs.getTimestamp("CreatedAt"));
+                assets.add(asset);
+            }
+            return assets;
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+        return null;
+
+    }
+
+    public List<Asset> getAssetsByStatus(boolean status) throws SQLException {
+        List<Asset> assets = new ArrayList<>();
+        String query = "SELECT * FROM Asset WHERE Status = ?";
+
+        try {
+            PreparedStatement pstmt = connection.prepareStatement(query);
+            pstmt.setBoolean(1, status);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                Asset asset = new Asset();
+                asset.setId(rs.getInt("AssetId"));
+                asset.setCustomerId(rs.getInt("CustomerId"));
+                asset.setImage(rs.getString("Image"));
+                asset.setDescription(rs.getString("Description"));
+                asset.setValue(rs.getBigDecimal("Value"));
+                asset.setVerification(rs.getBoolean("Verification"));
+                asset.setStatus(rs.getBoolean("Status"));
+                asset.setCreatedAt(rs.getTimestamp("CreatedAt"));
+                assets.add(asset);
+            }
+             return assets;
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+        return null;
+
+    }
+        public List<Asset> getAssetsByVerify(boolean status) throws SQLException {
+        List<Asset> assets = new ArrayList<>();
+        String query = "SELECT * FROM Asset WHERE Verification = ?";
+
+        try {
+            PreparedStatement pstmt = connection.prepareStatement(query);
+            pstmt.setBoolean(1, status);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                Asset asset = new Asset();
+                asset.setId(rs.getInt("AssetId"));
+                asset.setCustomerId(rs.getInt("CustomerId"));
+                asset.setImage(rs.getString("Image"));
+                asset.setDescription(rs.getString("Description"));
+                asset.setValue(rs.getBigDecimal("Value"));
+                asset.setVerification(rs.getBoolean("Verification"));
+                asset.setStatus(rs.getBoolean("Status"));
+                asset.setCreatedAt(rs.getTimestamp("CreatedAt"));
+                assets.add(asset);
+            }
+             return assets;
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+        return null;
+
+    }
+
+    public static void main(String[] args) {
+        UserDAO dao = new UserDAO();
+//        Asset a = dao.getAssetById(2)
+//        a.setVerification(true);
+//        a.setStatus(true);
+//        dao.updateAsset(a);
+//        System.out.println(a.toString());
+
     }
 }
