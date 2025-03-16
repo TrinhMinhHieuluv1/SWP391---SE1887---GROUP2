d1c2aeeb906f221a28c58ef1fd680896e56a7292
NgoVanCuongSE181200 <CuongNVHE181200@fpt.edu.vn>
2025-03-16 16:24:01 +0700

    detail bill

---
+++ FPTBank/src/java/controller/billprovdider/management/DetailBill.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package controller.billprovdider.management;
+
+import dal.CompanyBillProviderDAO;
+import dal.DetailBillDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.CompanyBillProvider;
+
+
+/**
+ *
+ * @author ACER
+ */
+@WebServlet(name="DetailBill", urlPatterns={"/bill_provider/billdetail"})
+public class DetailBill extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet DetailBill</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet DetailBill at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+       CompanyBillProviderDAO dao = new CompanyBillProviderDAO();
+       DetailBillDAO ddao = new DetailBillDAO();
+       String billID_raw = request.getParameter("billID");
+       String providerID_raw = request.getParameter("providerID");
+       int billID = Integer.parseInt(billID_raw);
+       int providerID = Integer.parseInt(providerID_raw);
+       CompanyBillProvider a = dao.getCompanyById("ProviderID",providerID);
+        model.DetailBill bill = ddao.getDetailBillByID("BillID", billID);
+       request.setAttribute("company", a);
+       request.setAttribute("billdetail", bill);
+       request.getRequestDispatcher("invoicedetail.jsp").forward(request, response);
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
