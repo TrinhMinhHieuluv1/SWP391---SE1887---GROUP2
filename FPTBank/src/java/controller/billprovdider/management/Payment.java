996ab1b099ce182ad25d16fdb517b9d25942e3a5
NgoVanCuongSE181200 <CuongNVHE181200@fpt.edu.vn>
2025-03-18 13:44:18 +0700

    add payment method

--- FPTBank/src/java/controller/billprovdider/management/Payment.java
+++ FPTBank/src/java/controller/billprovdider/management/Payment.java
@@ -4,6 +4,7 @@
  */
 package controller.billprovdider.management;
 
+import controller.vnpay.com.Config;
 import dal.CompanyBillProviderDAO;
 import dal.CustomerDAO;
 import dal.DetailBillDAO;
@@ -19,7 +20,25 @@ import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import java.io.IOException; 
+import 
+        java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.TimeZone;
+
 import model.CompanyBillProvider;
 import model.Customer;
 
@@ -78,6 +97,16 @@ public class Payment extends HttpServlet {
         String providerID_raw = request.getParameter("providerID");
         String total_raw = request.getParameter("total");
         String action = request.getParameter("action");
+        String paymentmethod = request.getParameter("paymentmethod");
+        if(total_raw == null || total_raw.isEmpty()){
+            total_raw = "0";
+        }
+        if(billID_raw == null || billID_raw.isEmpty()){
+            billID_raw = "0";
+        }
+        if(providerID_raw == null || providerID_raw.isEmpty()){
+            providerID_raw = "0";
+        }
         double totall = Double.parseDouble(total_raw);
         int billID = Integer.parseInt(billID_raw);
         int providerID = Integer.parseInt(providerID_raw);
@@ -85,7 +114,7 @@ public class Payment extends HttpServlet {
         CompanyBillProvider company = cdao.getCompanyById("ProviderID", providerID);
         Customer customer = udao.selectCustomerByConditions(uid, "", "", "");
         String error = "";
-        if ("Paid".equals(action)) {
+        if ("Paid".equals(action) && "balance".equals(paymentmethod)) {
             BigDecimal total = BigDecimal.valueOf(totall);
             BigDecimal balance = customer.getBalance();
 
@@ -98,16 +127,81 @@ public class Payment extends HttpServlet {
                 bill.setStatusOfBill(0);
                 bill.setPaymentDate(paymentTimestamp);
                 customer.setBalance(balance);
-                
+
                 boolean mail = sendMailbillProvider.guiMailforPaying(customer.getEmail(), bill.getBillID(), bill.getTitle(), bill.getDescription(), bill.getStartDate(), bill.getEndDate(), bill.getEndDate(), bill.getTotal(), company.getCompanyName(), customer, paymentTimestamp);
-                if(mail){
+                if (mail) {
                     error = "Paid successfully and send invoice to email";
                     dao.updateDetailBill(bill);
                     udao.updateACustomer(customer);
-                }else {
+                } else {
                     error = "Don't send email";
                 }
             }
+        } else if ("Paid".equals(action) && "transfer".equals(paymentmethod)) {
+            String vnp_Version = "2.1.0";
+            String vnp_Command = "pay";
+            String orderType = "other";
+            long amount = (long) (totall * 100);
+            String vnp_TxnRef = billID + "";
+            String vnp_IpAddr = Config.getIpAddress(request);
+            String vnp_TmnCode = Config.vnp_TmnCode;
+            Map<String, String> vnp_Params = new HashMap<>();
+            vnp_Params.put("vnp_Version", vnp_Version);
+            vnp_Params.put("vnp_Command", vnp_Command);
+            vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
+            vnp_Params.put("vnp_Amount", String.valueOf(amount));
+            vnp_Params.put("vnp_CurrCode", "VND");
+            vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
+            vnp_Params.put("vnp_OrderInfo", "Thanh toan don hang:" + vnp_TxnRef);
+            vnp_Params.put("vnp_OrderType", orderType);
+
+            String locate = request.getParameter("language");
+            if (locate != null && !locate.isEmpty()) {
+                vnp_Params.put("vnp_Locale", locate);
+            } else {
+                vnp_Params.put("vnp_Locale", "vn");
+            }
+            vnp_Params.put("vnp_ReturnUrl", Config.vnp_ReturnUrl+"?total="+ bill.getTotal()+"&billID="+bill.getBillID()+"&providerID="+bill.getProvider().getUserID());
+            vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
+
+            Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+            String vnp_CreateDate = formatter.format(cld.getTime());
+            vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
+
+            cld.add(Calendar.MINUTE, 15);
+            String vnp_ExpireDate = formatter.format(cld.getTime());
+            vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
+
+            List fieldNames = new ArrayList(vnp_Params.keySet());
+            Collections.sort(fieldNames);
+            StringBuilder hashData = new StringBuilder();
+            StringBuilder query = new StringBuilder();
+            Iterator itr = fieldNames.iterator();
+            while (itr.hasNext()) {
+                String fieldName = (String) itr.next();
+                String fieldValue = (String) vnp_Params.get(fieldName);
+                if ((fieldValue != null) && (fieldValue.length() > 0)) {
+                    //Build hash data
+                    hashData.append(fieldName);
+                    hashData.append('=');
+                    hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                    //Build query
+                    query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
+                    query.append('=');
+                    query.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                    if (itr.hasNext()) {
+                        query.append('&');
+                        hashData.append('&');
+                    }
+                }
+            }
+            String queryUrl = query.toString();
+            String vnp_SecureHash = Config.hmacSHA512(Config.secretKey, hashData.toString());
+            queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
+            String paymentUrl = Config.vnp_PayUrl + "?" + queryUrl;
+            response.sendRedirect(paymentUrl);
+            return;
         }
         request.setAttribute("error", error);
         request.setAttribute("customer", customer);
